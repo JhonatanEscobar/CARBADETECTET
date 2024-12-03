4d8bf92e3ff1742408bfc56fa96ceaebcbfd366f
JhonatanEscobar <jhontanadriangutierrez@gmail.com>
2024-12-03 02:10:19 -0400

    waaa
    waaa

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('download-button').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = 'CarbaDetectetInstaller.exe'; // Reemplaza con el nombre real del archivo
+    link.download = 'CarbaDetectetInstaller.exe'; // Nombre sugerido para la descarga
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
