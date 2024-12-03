d650b70d2dfbfda0fa4a7b6780bf3173b5bd78c9
JhonatanEscobar <jhontanadriangutierrez@gmail.com>
2024-12-03 02:58:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ burger.addEventListener('click', () => {
 // Botón de descarga
 document.getElementById('download-button').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'CarbaDetectetInstaller.exe'; // Reemplaza con el nombre real del archivo
-    link.download = 'CarbaDetectetInstaller.exe'; // Nombre sugerido para la descarga
+    link.href = 'CARBADETECTET.exe'; // Reemplaza con el nombre real del archivo
+    link.download = 'CARBADETECTET.exe'; // Nombre sugerido para la descarga
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
