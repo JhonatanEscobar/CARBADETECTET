861b7ce35cbe4b2cdf08abf52e9818531527916e
JhonatanEscobar <jhontanadriangutierrez@gmail.com>
2024-12-03 03:44:54 -0400

    wa
    wa

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById('download-button').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = './CARBADETECTET.exe'; // Ruta relativa asegurada
-    link.download = 'CARBADETECTET.exe'; // Nombre sugerido para la descarga
+    link.href = './CARBADETECTET.zip'; // Asegúrate de que la ruta sea correcta
+    link.download = 'CARBADETECTET.zip'; // Nombre sugerido para la descarga
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
