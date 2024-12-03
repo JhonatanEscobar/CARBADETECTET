d7384a2e98d6acae0665d536419296e87db3feea
JhonatanEscobar <jhontanadriangutierrez@gmail.com>
2024-12-03 02:40:46 -0400

    WA
    WA

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// Navegación responsive
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
+
+// Botón de descarga
 document.getElementById('download-button').addEventListener('click', function() {
     const link = document.createElement('a');
     link.href = 'CarbaDetectetInstaller.exe'; // Reemplaza con el nombre real del archivo
