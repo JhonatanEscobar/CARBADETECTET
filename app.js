c6c6d1be34c3902e8535e8af6c1f5167d3cfc49c
JhonatanEscobar <jhontanadriangutierrez@gmail.com>
2024-12-03 03:40:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-// Navegación responsive
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
-});
-
-// Botón de descarga
 document.getElementById('download-button').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'CARBADETECTET.exe'; // Reemplaza con el nombre real del archivo
+    link.href = './CARBADETECTET.exe'; // Ruta relativa asegurada
     link.download = 'CARBADETECTET.exe'; // Nombre sugerido para la descarga
     document.body.appendChild(link);
     link.click();
